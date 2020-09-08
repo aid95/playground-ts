c7c8adfaf3cedae5b8f942bc048cbe5db662ec1b
imgomi <webmaster@mail.gomi.land>
2020-09-08 17:44:17 +0900

    Types in Typescript

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const name = "imgomi", age = 20, gender = "male";
+
+const sayHi = (name: string, age: number, gender: string): void => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}!`);
+};
+
+sayHi(name, age, gender);
+
+export {};
