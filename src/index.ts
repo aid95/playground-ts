aff0773456e0d30e8108a744ac1a68e211e3902a
imgomi <webmaster@mail.gomi.land>
2020-09-08 18:05:08 +0900

    Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-const name = "imgomi", age = 20, gender = "male";
-
-const sayHi = (name: string, age: number, gender: string): void => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}!`);
+interface Human {
+  name: string,
+  age: number,
+  gender: string
 };
 
-sayHi(name, age, gender);
+const person = {
+  name: "imgomi",
+  age: 20,
+  gender: "male"
+};
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 
 export {};
